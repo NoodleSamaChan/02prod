117059ad00276340aec16e5a767357e51aebb251
NoodleSamaChan <lferraraccio@outlook.com>
2024-05-26 15:42:41 +0200

    finished implementation logs

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use sqlx::{PgPool};
 use zer02prod::{configuration::get_configuration, startup::run};
 use zer02prod::telemetry::{get_subscriber, init_subscriber};
 use std::net::TcpListener;
+use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let subscriber = get_subscriber("zer02prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection = PgPool::connect(&configuration.database.connection_string())
+    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
--- src/startup.rs
+++ src/startup.rs
@@ -2,15 +2,15 @@ use actix_web::dev::Server;
 use std::net::TcpListener;
 use actix_web::{HttpServer, App, web};
 use crate::routes::{health_check, subscribe};
-use actix_web::middleware::Logger;
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
