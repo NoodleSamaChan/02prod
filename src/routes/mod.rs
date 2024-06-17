c4610857a9bb351b9b77e02d6f8c64ad02b73f4a
NoodleSamaChan <lferraraccio@outlook.com>
2024-06-17 16:19:16 +0200

    worked on sending newsletter

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,9 @@
 mod health_check;
+mod newsletters;
 mod subscriptions;
 mod subscriptions_confirms;
 
 pub use health_check::*;
+pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirms::*;
