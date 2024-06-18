98dca8a98eb92a4d0be60666bbeb4208b326187d
NoodleSamaChan <lferraraccio@outlook.com>
2024-06-18 13:55:49 +0200

    started adding password security

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,5 +1,4 @@
 use crate::helpers::{spawn_app, ConfirmationLinks, TestApp};
-use actix_web::test;
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 #[tokio::test]
@@ -120,4 +119,30 @@ async fn newsletters_returns_400_for_invalid_data() {
             error_message
         )
     }
+}
+
+#[tokio::test]
+async fn requests_missing_authorization_are_rejected() {
+    // Arrange
+    let app = spawn_app().await;
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
 }
