35d8b95fb7e660730007d8d022504b79eeb206a4
NoodleSamaChan <lferraraccio@outlook.com>
2024-06-20 12:50:02 +0200

    added safety around login

--- src/startup.rs
+++ src/startup.rs
@@ -10,6 +10,7 @@ use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+use secrecy::Secret;
 
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPoolOptions::new().connect_lazy_with(configuration.with_db())
@@ -22,6 +23,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -39,12 +41,17 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
 
     Ok(server)
 }
+
+
+#[derive(Clone)]
+pub struct  HmacSecret(pub Secret<String>);
 
 pub struct Application {
     port: u16,
@@ -81,6 +88,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
